49f48ce5c42ce6f05f663972d3beabcc7ef17446
server <server@server.com>
2021-02-05 16:34:07 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,23 @@
+function  myStyleNumber(){
+     document.getElementById("lebel").style.display="block";
+     document.getElementByTagName("number").sty
+  
+}
+function  myStyleName(){
+     document.getElementById("lebel2").style.display="block";
+  
+}
+function  myStyleEmail(){
+    document.getElementById("lebel3").style.display="block";
+ 
+}
+console.log("xyz");
+function validateNumber(){
+    var myNumber =document.getElementById("number").value;
+   console.log("myNumber");
+  
+    if (  myNumber.match("/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/")){
+        document.getElementByTagName(small).style.visibility="hidden";
+    }
+    // else{
+    //     document.getElementsByClassName(".mycontrol.success 
